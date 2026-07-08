c3b0dcb6bf21271df289db3c57d188e3612da540
Noah Poiré <noah.poire@etu.cyu.fr>
2026-07-08 19:31:17 +0200

    Commit requirements

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
   Wire.begin(21, 22); // SDA, SCL
   Serial.println("Scan I2C...");
 
-  for (byte addr = 1; addr < 127; addr++) {
+  for (byte addr = 1; addr < 127; addr++) 
+  {
     Wire.beginTransmission(addr);
-    if (Wire.endTransmission() == 0) {
+    if (Wire.endTransmission() == 0) 
+    {
       Serial.print("Trouvé à 0x");
       Serial.println(addr, HEX);
     }
@@ -15,4 +18,7 @@ void setup() {
   Serial.println("Terminé.");
 }
 
-void loop() {}
+void loop() 
+{
+  
+}
